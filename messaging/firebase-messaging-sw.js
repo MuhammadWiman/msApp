1bcb9e9da2239b4d20f9cb7bca0c03695f2f3fac
MuhammadWiman <73020035+MuhammadWiman@users.noreply.github.com>
2021-01-09 15:13:07 +0700

    Update firebase-messaging-sw.js

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 ﻿// Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/6.4.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.4.2/firebase-messaging.js');
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
