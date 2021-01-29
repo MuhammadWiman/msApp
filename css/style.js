182c99043e92441dab0ce09a5442c139d4179ca0
MuhammadWiman <73020035+MuhammadWiman@users.noreply.github.com>
2021-01-29 14:28:49 +0700

    Update and rename style.css to style.js

---
+++ css/style.js
@@ -0,0 +1,148 @@
+var styles = {
+
+//body, html
+    bodyElement: {
+            width: "100%",
+            height: "100%",
+            padding: "0",
+            margin: "0",
+            overflow: "hidden"
+    }
+    htmlElement: {
+            width: "100%",
+            height: "100%",
+            padding: "0",
+            margin: "0",
+            overflow: "hidden"
+    },
+//body
+    bodyElement: {
+            fontFamily: "'Segoe UI', Helvetica Nenu, Helvetica, 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+            backgroundImage: "linear-gradient(180deg, #dddbd1, #d2dbdc)"
+    },
+//.top
+    topClass: {
+            height: "130px",
+            width: "100%",
+            backgroundColor: "#009688",
+            position: "fixed",
+            zIndex: "-1"
+    },
+//.chatbox
+    chatboxClass: {
+            height: "calc(100% - 40px)",
+            width: "95%",
+            marginTop: "20px",
+            padding: "0"
+    },
+//.profile-pic
+    profilePicClass: {
+            height: "40px",
+            width: "40px"
+    },
+//.form-rounded
+    formRoundedClass: {
+            borderRadius: "1rem"
+    },
+//.friend-pic
+    friendPicClass: {
+            height: "50px",
+            width: "50px",
+            marginLeft: "-10px"
+    },
+//.name
+    nameClass: {
+            fontWeight: "400",
+            color: "#000"
+    },
+//.under-name
+    underNameClass: {
+            fontSize: "12px",
+            lineHeight: "15px",
+            maxHeight: "15px"
+    },
+//.icon
+    iconClass: {
+            fontSize: "20px",
+            color: "#9CA1A3",
+            cursor: "pointer"
+    },
+//.chat-pic
+    chatPicClass: {
+            height: "30px",
+            width: "30px"
+    },
+//.receive
+    receiveClass: {
+            backgroundColor: "lightgreen",
+            borderRadius: "1rem",
+            padding: "10px 15px",
+            display: "inline-block"
+    },
+//.sent
+    sentClass: {
+            backgroundColor: "whitesmoke",
+            borderRadius: "1rem",
+            padding: "10px 15px",
+            display: "inline-block"
+    },
+//.time
+    timeClass: {
+            fontSize: "10px",
+            color: "#9CA1A3",
+            marginTop: "3%",
+            marginLeft: "1%"
+    },
+//.card
+    cardClass: {
+            height: "94vh"
+    },
+//#messages
+    messagesId: {
+            overflowY: "scroll",
+            overflowX: "hidden"
+    },
+//#lstChat
+    lstChatId: {
+            overflowY: "scroll",
+            overflowX: "hidden"
+    },
+//.dropdown-toggle::before
+    dropdownToggleBeforeClass: {
+            display: "none !important"
+    },
+//.dropdown-toggle::after
+    dropdownToggleAfterClass: {
+            display: "none !important"
+    },
+//#emoji
+    emojiId: {
+            position: "absolute",
+            top: "-30vh",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            boxShadow: "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0,0,0,0.23) 0px 3px 6px"
+    },
+//#emoji #myTabContent
+    emojiMyTabContentId: {
+            height: "20vh"
+    },
+//#myTabContent a
+    myTabContentaId: {
+            textDecoration: "none"
+    },
+//#notification
+    notificationId: {
+            backgroundColor: "red",
+            border: "1px solid red",
+            borderRadius: "50%",
+            position: "absolute",
+            marginTop: "-10%",
+            fontSize: "12px",
+            width: "30%",
+            textAlign: "center",
+            color: "white",
+            fontWeight: "bold",
+            marginLeft: "-12%"
+    }
+}
